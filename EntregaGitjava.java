17fe6701b74c2ac607b544f9dcf111b813d88cfc
Pedro <pedrofuruguem@hotmail.com>
2021-02-13 20:30:27 -0300

    primeira entrega teste

---
+++ EntregaGitjava.java
@@ -0,0 +1,10 @@
+//import java.swing.JOptionPane;
+public class EntregaGit{
+    public static void main(String[] args){
+        String = nome = JOptionPane.ShowinputDialog("Qual o seu nome?");
+        JOptionPane.showMessageDialog(null, String.format("Bem vindo, %s", nome;))
+        //outra funcionalidade
+        //mais uma 
+        //outra
+    }
+}
